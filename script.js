069ec9d9516fb1cd071a8727e09bcce50a63bd21
Ꮥʜꫝʟɪɴɪ <155555305+Itz-SHALINI@users.noreply.github.com>
2024-07-19 18:33:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
-const buttons = document.querySelectorAll(".card-buttons button");
-const sections = document.querySelectorAll(".card-section");
-const card = document.querySelector(".card");
-
-const handleButtonClick = e => {
-  const targetSection = e.target.getAttribute("data-section");
-  const section = document.querySelector(targetSection);
-  
-  if (!e.target.classList.contains("is-active")) {
-    card.setAttribute("data-state", targetSection);
-    sections.forEach(s => s.classList.remove("is-active"));
-    buttons.forEach(b => b.classList.remove("is-active"));
-    e.target.classList.add("is-active");
-    section.classList.add("is-active");
-  }
-};
-
-buttons.forEach(btn => {
-  btn.addEventListener("click", handleButtonClick);
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.querySelector('.loader').style.display = 'none';
+        document.getElementById('portfolio-content').style.display = 'block';
+    }, 3000);
 });
 
-const toggleSwitch = document.getElementById('background-toggle');
-const body = document.body;
-
-toggleSwitch.addEventListener('change', () => {
-  if (toggleSwitch.checked) {
-    body.style.backgroundImage = "url('S/NightMode.png')";
-  } else {
-    body.style.backgroundImage = "url('S/DayMode.png')";
-  }
+var typed = new Typed('#element', {
+    strings: ['Python Bot Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
 });
